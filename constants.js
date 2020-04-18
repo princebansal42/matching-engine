aedbe573cbac10a0682c653c431b51cb5fbf05c7
princebansal42 <princebansal42@gmail.com>
2020-04-18 17:34:18 +0530

    Constants Added

---
+++ constants.js
@@ -0,0 +1,19 @@
+const ORDER_TYPE = {
+    LIMIT_ORDER: "LIMIT_ORDER",
+    MARKET_ORDER: "MARKET_ORDER",
+};
+const ACTION = {
+    BUY: "BUY",
+    SELL: "SELL",
+};
+
+const STATUS = {
+    PENDING: "PENDING",
+    COMPLETED: "COMPLETED",
+};
+
+module.exports = {
+    ORDER_TYPE,
+    ACTION,
+    STATUS,
+};
